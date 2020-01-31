34982d5e0e3cb35ef7e86e48e4be05885732f009
Michael George <georgem@wlwv.k12.or.us>
2020-01-31 09:12:59 -0800

    first commit

---
+++ src/FrequencyChart.java
@@ -0,0 +1,16 @@
+public class FrequencyChart {
+    private int oneToTenCt = 0;
+    private int elevenToTwentyCt = 0;
+    private int twenOneToThirtyCt = 0;
+    private int thirtyOneToFortyCt = 0;
+    private int fortyOneToFiftyCt = 0;
+    private int fiftyOneToSixtyCt = 0;
+    private int sixtyOneToSeventyCt = 0;
+    private int seventyOneToEightyCt = 0;
+    private int eightyOneToNinetyCt = 0;
+    private int ninetyOneToHundredyCt = 0;
+
+
+
+
+}
