f9bdd84f36a752cdeef71a75d6b68026293a3af6
Michael George <georgem@wlwv.k12.or.us>
2020-02-25 09:49:18 -0800

    Added ArrayList Practice

--- src/Array2DExplorer.java
+++ src/Array2DExplorer.java
@@ -6,17 +6,17 @@ public class Array2DExplorer {
                 {6,6,2,2} };
         Array2DExplorer exp = new Array2DExplorer();
 
+        //evenRow Tester:
         System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array,0));
         System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array,2));
-
         System.out.println();
 
+        //oddColSum Tester:
         System.out.println("Test oddColSum: \n Col w/ odd Sum, Expecting: true \n Actual: " + exp.oddColSum(array,1));
         System.out.println("Test oddColSum: \n Col w/ even, Expecting: false \n Actual: " + exp.oddColSum(array,0));
-
         System.out.println();
 
-
+        //minRow Tester:
         System.out.print("Test minRow: \n Expecting: {0 2 3 4} \n Actual: {");
         int [] row = exp.minRow(array);
         for(int i = 0; i < row.length; i++){
@@ -24,9 +24,48 @@ public class Array2DExplorer {
         }
         System.out.print("}");
 
+        //minRowInt Tester:
+        System.out.println("Test minRowInt: \nExpecting: 0 \n Actual: " + exp.minRowInt(array, 1));
+        System.out.println();
+
+        //colMax Tester:
+        System.out.println("Test colMax: \nExpecting: {6, 6, 8, 5} \n Actual: {");
+        int [] colArr = exp.colMax(array);
+        for(int i = 0; i < colArr.length; i++){
+            System.out.print(colArr[i] + " ");
+        }
+        System.out.print("}");
+        System.out.println();
+
+        //allRowSums Tester:
+        System.out.println("Test allRowSums: \nExpecting: {18, 9, 16} \n Actual: {");
+        int [] sumArr = exp.allRowSums(array);
+        for(int i = 0; i < sumArr.length; i++){
+            System.out.print(sumArr[i] + " ");
+        }
+        System.out.print("}");
+        System.out.println();
+
+        //averageCol Tester:
+        System.out.println("Test averageCol: \nExpecting: {3.333333, 3, 4.333333, 3.666667} \n Actual: {");
+        double [] avgColArr = exp.averageCol(array);
+        for(int i = 0; i < avgColArr.length; i++){
+            System.out.print(avgColArr[i] + " ");
+        }
+        System.out.print("}");
+        System.out.println();
+
+        //smallEven Tester:
+        System.out.println("Test smallEven: \nExpecting: 2 \n Actual: " + exp.smallEven(array));
+        System.out.println();
+        //returns the row index number that has the greatest sum of its elements.
+        //biggestRow Tester:
+        System.out.println("Test biggestRow: \nExpecting: 0 \n Actual: " + exp.biggestRow(array));
+        System.out.println();
     }
 
     //WRITE YOUR METHODS BELOW
+
 
     //returns true if all the numbers in the given row are even
     public boolean evenRow(int [][] mat, int row){
@@ -74,7 +113,7 @@ public class Array2DExplorer {
     }
 
     //looks through each element of the chosen row and returns the smallest integer from that row
-    public int minRow(int[][] nums, int row){
+    public int minRowInt(int[][] nums, int row){
         int currentLow = nums[row][0];
         for (int i = 0; i < nums[row].length; i++) {
             if (nums[row][i] < currentLow){
@@ -85,7 +124,7 @@ public class Array2DExplorer {
     }
 
     //searches through each column of an array and returns an array with the largest integer from each column
-    public int[] colMaxs(int[][] matrix){
+    public int[] colMax(int[][] matrix){
         int[] arr = new int[matrix[0].length];
         for (int j = 0; j < matrix[0].length; j++) {
             int high = matrix[0][j];
@@ -127,12 +166,40 @@ public class Array2DExplorer {
 
     //finds and returns the smallest even number in the array matrix. Assume that the array is filled with only positive ints
     public int smallEven(int[][] matrix){
+        int low = 100;
 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] % 2 == 0 && matrix[i][j] < low && matrix[i][j] != 0){
+                    low = matrix[i][j];
+                }
+            }
+        }
+
+        return low;
     }
 
     //returns the row index number that has the greatest sum of its elements.
-    public static int biggestRow(int[][] nums){
+    public int biggestRow(int[][] nums){
+        int maxRow = 0;
+        int currentSum = 0;
+        for (int i = 0; i < nums[0].length; i++) {
+            currentSum += nums[0][i];
+        }
 
+        int compareSum;
+        for (int i = 1; i < nums.length; i++) {
+            compareSum = 0;
+            for (int j = 0; j < nums[i].length ; j++) {
+                compareSum += nums[i][j];
+            }
+            if (compareSum > currentSum){
+                currentSum = compareSum;
+                maxRow = i;
+            }
+        }
+
+        return maxRow;
     }
 
 
